e212571ea719f0a3a7e0ff8345ee67e73872c2d0
goadeveloper <gosoft2005@gmail.com>
2024-11-19 14:38:01 -0500

    Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ComponenteEnLineaComponent } from "./componente-en-linea/componente-en-linea.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ComponenteEnLineaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
