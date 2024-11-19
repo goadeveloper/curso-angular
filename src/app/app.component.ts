4cc977487941f1630c097d70628b29ef296c9075
goadeveloper <gosoft2005@gmail.com>
2024-11-19 15:15:50 -0500

    Child components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ComponenteEnLineaComponent } from "./componente-en-linea/componente-en-linea.component";
-import { InterpolacionComponent } from "./interpolacion/interpolacion.component";
+import { PadreComponent } from "./padre/padre.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ComponenteEnLineaComponent, InterpolacionComponent],
+  imports: [RouterOutlet, PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Hola mundo desde Angular';
+  title = 'Curso Angular';
 }
